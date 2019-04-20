40631c5b734cbb6e863d1d114aba3f066c2aed09
Emre Serbest <eserbest1903@gmail.com>
2019-04-20 22:35:16 +0300

    how async method works

---
+++ how-async-method-works/async.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+void main(List<String> args) {
+
+print("Program başlatıldı Main method başlatıldı");
+dosyaIceriginiGoster();
+print("Main methoda geri dönüldü bitti!!!");
+
+}
+
+/* Aynı sonucu verir 
+dosyaIceriginiGoster() {  
+print("Dosya içeriği gösterilecek");
+
+Future <String> dosyaIcerigi =  dosyaIndir(); 
+dosyaIcerigi.then((sonuc) => print("$sonuc")); // return yapıldıktan sonra bu satır çalıştırılır
+
+//print("DOSYA İÇERİĞİ >>>  $dosyaIcerigi");
+}
+*/
+
+dosyaIceriginiGoster() async{  // await keywordu ancak async olan methodlarda kullanılabilir
+
+print("Dosya içeriği gösterilecek");
+String dosyaIcerigi = await dosyaIndir(); // Bu işlem ana senkronun dışına çıkarak işlem tamamlanasıya kadar "thread" e benzer yapıda arkaplanda çalıştırılır.
+print("DOSYA İÇERİĞİ >>>  $dosyaIcerigi");
+
+}
+
+Future<String> dosyaIndir(){
+print("Dosya indirme işlemi başlatıldı...");
+
+Future<String> sonuc = Future.delayed(Duration(seconds: 10),(){
+return "*******İndirilen dosya içeriği******";
+});
+
+print("Dosya indir methodu tamamlandı...!");
+return sonuc;
+}
+
+/* OUTPUT 
+
+Program başlatıldı Main method başlatıldı
+Dosya içeriği gösterilecek
+Dosya indirme işlemi başlatıldı...
+Dosya indir methodu tamamlandı...!
+Main methoda geri dönüldü bitti!!!
+*******İndirilen dosya içeriği****** /// 5 saniye bekledikten sonra bu satır yazdırılıyor.
+
+*/
